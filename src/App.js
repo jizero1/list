1f7adb91841a369caea333ca5920ebda35bad97c
jizero4101@naver.com <jizero4101@naver.com>
2024-09-12 01:04:49 +0900

    컴포넌트 위치수정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, {Component, useEffect} from 'react';
 import logo from './logo.svg'; 
 import './App.css';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
+import styled from 'styled-components';
 
 function App() {
   
   // const today = new Date();
   // const date = today.toLocaleDateString();
-  const [input, setInput] = useState('');
   const [post, setPost] = useState([]);
-  let [btn, setBtn] = useState(false);
-  const [clickEvent,setClickEvent] = useState('');
+  const [pop, setPop] = useState('');
+
 
 
   return (
@@ -21,41 +21,89 @@ function App() {
           <h1 className="list-h1">To do list</h1>
           {/* <p className="date">{date}</p> */}
           {/* <hr></hr> */}
-          <div className="list-ib">
-            <input className="list-input" onChange = {(e) => {
+          {/* <div className="list-ib">
+          <input className="list-input" onChange = {(e) => {
               const inputValue = e.target.value;
               setInput(inputValue);
-            }}></input>
+            }} placeholder="오늘 할 일을 입력해주세요!"></input>
     
             <button className="list-button" onClick = {() => {
               const inputPost = [...post];
               inputPost.unshift(input);
               setPost(inputPost);
             }}>💙</button>
-          </div>
+          </div> */}
+          <Insert post={post} setPost={setPost}></Insert>
+          
           <hr className="list-hr"></hr>
-          <div className="list-post">
+          {/* <div className="list-post">
             {
             post.map(function(a,i) {
               return (
               <div clasName="post" key={i}>
                 <div className="post-p">{post[i]}</div>
-                <div className="post-btn" onClick={(e)=>{
-                  let 누른버튼 = e.target;
-                  console.log(누른버튼);
-                }}></div>
+                <label>
+                  <input type="checkbox"></input>
+                </label>
               </div>
             )
             })
             }
             
-          </div>
+          </div> */}
+          <InsertView post={post}></InsertView>
         </div>
       </div>
     </div>
+
+    
   );
 }
 
+// 할일 입력창 + 제출버튼
+function Insert(props) {
+  const [input, setInput] = useState('');
+
+  const onSubmit = useCallback((e) => {
+    setInput('');
+    e.preventDefault();
+  });
+  return (
+    <div className="list-ib">
+      <form onSubmit={onSubmit}> 
+      <input value={input} className="list-input" onChange = {(e) => {
+        setInput(e.target.value);
+        }} placeholder="오늘 할 일을 입력해주세요!"></input>
+    
+      <button className="list-button" onClick = {() => {
+        const inputPost = [...props.post];
+        inputPost.unshift(input);
+        props.setPost(inputPost);
+      }}>💙</button>
+      </form>
+    </div>
+  )
+}
+
+// 입력한 내용보여주는곳
+function InsertView(props) {
+  return (
+    <div className="list-post">
+    {
+    props.post.map(function(a,i) {
+
+      <div clasName="post" key={i}>
+          <input className="post-chk" type="checkbox" id="chk"></input>
+          <label for="chk"></label>
+          <label for="chk" className="post-p">{props.post[i]}</label>
+
+      </div>
+
+    })
+    }
+  </div>
+  )
+}
 
 export default App;
 
