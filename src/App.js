eca6351e53a10615b5b4dde4198188be0227677d
jizero4101@naver.com <jizero4101@naver.com>
2024-09-15 17:41:21 +0900

    삭제추가

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 import { useState, useCallback } from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX, faCheck } from "@fortawesome/free-solid-svg-icons";
+
+// const data = [
+//   {
+//     id: 0,
+//     content: "고구마먹기",
+//   },
+//   {
+//     id: 1,
+//     content: "삼각김밥 사러가기",
+//   }
+// ]
 
 function App() {
-  
-  // const today = new Date();
-  // const date = today.toLocaleDateString();
+
   const [post, setPost] = useState([]);
-  const [pop, setPop] = useState('');
-
-
+  const [input, setInput] = useState('');
+  // const [count, setCount] = useState(0);
 
   return (
     <div className="container">
@@ -19,67 +29,54 @@ function App() {
         <div className="list-container2">
           <p className="line">|</p>
           <h1 className="list-h1">To do list</h1>
-          {/* <p className="date">{date}</p> */}
-          {/* <hr></hr> */}
-          {/* <div className="list-ib">
-          <input className="list-input" onChange = {(e) => {
-              const inputValue = e.target.value;
-              setInput(inputValue);
-            }} placeholder="오늘 할 일을 입력해주세요!"></input>
-    
-            <button className="list-button" onClick = {() => {
-              const inputPost = [...post];
-              inputPost.unshift(input);
-              setPost(inputPost);
-            }}>💙</button>
-          </div> */}
-          <Insert post={post} setPost={setPost}></Insert>
-          
+          <Insert post={post} setPost={setPost} input={input} setInput={setInput}></Insert>
           <hr className="list-hr"></hr>
-          {/* <div className="list-post">
-            {
-            post.map(function(a,i) {
-              return (
-              <div clasName="post" key={i}>
-                <div className="post-p">{post[i]}</div>
-                <label>
-                  <input type="checkbox"></input>
-                </label>
-              </div>
-            )
-            })
-            }
-            
-          </div> */}
-          <InsertView post={post}></InsertView>
+          <InsertView post={post} setPost={setPost} input={input} setInput={setInput}></InsertView>
         </div>
       </div>
-    </div>
-
-    
+    </div>  
   );
 }
 
 // 할일 입력창 + 제출버튼
 function Insert(props) {
-  const [input, setInput] = useState('');
 
+  // 제출버튼 누르면, input란을 비움
   const onSubmit = useCallback((e) => {
-    setInput('');
+    props.setInput('');
     e.preventDefault();
+
+    // props.post.map(function(a,i) {
+    //   props.setCount(i+1);
+    // })
   });
+  const onChange = useCallback((e) => {
+    props.setInput(e.target.value);
+  })
+  const onClick = useCallback((e) => {
+    if (props.input != '' && props.post.length<9) {
+      const inputPost = [...props.post];
+      inputPost.push(props.input)
+      props.setPost(inputPost);
+    }
+    else if (props.input == '') {
+      alert("할일을 입력해주세요!");
+    }
+    else {
+      alert("할일이 꽉 찼어요!");
+    }
+
+    // let 입력 = props.input;
+    // let 배열 = props.post;
+    // if (배열.includes(입력)==true) {
+    //   alert("이미 할일에 추가되어있어요!");
+    // }
+  })
   return (
     <div className="list-ib">
       <form onSubmit={onSubmit}> 
-      <input value={input} className="list-input" onChange = {(e) => {
-        setInput(e.target.value);
-        }} placeholder="오늘 할 일을 입력해주세요!"></input>
-    
-      <button className="list-button" onClick = {() => {
-        const inputPost = [...props.post];
-        inputPost.unshift(input);
-        props.setPost(inputPost);
-      }}>💙</button>
+        <input value={props.input} className="list-input" onChange={onChange} placeholder="오늘 할 일을 입력해주세요!"></input>
+        <button className="list-button" onClick={onClick}>💙</button>
       </form>
     </div>
   )
@@ -87,18 +84,35 @@ function Insert(props) {
 
 // 입력한 내용보여주는곳
 function InsertView(props) {
+
+  const [바꿈,set바꿈] = useState('');
+
   return (
     <div className="list-post">
     {
     props.post.map(function(a,i) {
 
+      const onClick = () => {
+        set바꿈('postAdd');
+      }
+      const onRemove = () => {
+        const clickData = props.post[i]; //현재 클릭한값 저장. 
+        const postData = props.post; //post 배열 전체값.
+        // filter을 이용하여, 현재 선택한값이 post배열안에 있으면, 걔만빼고 배열재구성
+        props.setPost(postData.filter((e) => e !== clickData));
+      }
+
+      return (
       <div clasName="post" key={i}>
-          <input className="post-chk" type="checkbox" id="chk"></input>
-          <label for="chk"></label>
-          <label for="chk" className="post-p">{props.post[i]}</label>
-
+          <input className={'post-chk '+바꿈} type="checkbox" id={'chk'+i} onClick={onClick}></input>
+          <label for={'chk'+i}>
+          <div className="font"><FontAwesomeIcon icon={faCheck} ></FontAwesomeIcon></div>
+          </label>
+          <label for={'chk'+i} className="post-p">{props.post[i]}</label>
+          <button className="post-remove"onClick={onRemove}><FontAwesomeIcon icon={faX}></FontAwesomeIcon></button>
+          {/* <input type="text"></input> */}
       </div>
-
+      )
     })
     }
   </div>
